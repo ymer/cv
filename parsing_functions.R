a191ad4781da79b637f7b5b35fea4ed9bad6651c
ymer <jonatan.smp@gmail.com>
2019-10-23 11:12:46 +0200

    edits

--- parsing_functions.R
+++ parsing_functions.R
@@ -1,49 +1,3 @@
-# Regex to locate links in text
-find_link <- regex("
-  \\[   # Grab opening square bracket
-  .+?   # Find smallest internal text as possible
-  \\]   # Closing square bracket
-  \\(   # Opening parenthesis
-  .+?   # Link text, again as small as possible
-  \\)   # Closing parenthesis
-  ",
-                   comments = TRUE)
-
-# Function that removes links from text and replaces them with superscripts that are 
-# referenced in an end-of-document list. 
-sanitize_links <- function(text){
-  if(PDF_EXPORT){
-    str_extract_all(text, find_link) %>% 
-      pluck(1) %>% 
-      walk(function(link_from_text){
-        title <- link_from_text %>% str_extract('\\[.+\\]') %>% str_remove_all('\\[|\\]') 
-        link <- link_from_text %>% str_extract('\\(.+\\)') %>% str_remove_all('\\(|\\)')
-        
-        # add link to links array
-        links <<- c(links, link)
-        
-        # Build replacement text
-        new_text <- glue('{title}<sup>{length(links)}</sup>')
-        
-        # Replace text
-        text <<- text %>% str_replace(fixed(link_from_text), new_text)
-      })
-  }
-  text
-}
-
-# Take entire positions dataframe and removes the links 
-# in descending order so links for the same position are
-# right next to eachother in number. 
-strip_links_from_cols <- function(data, cols_to_strip){
-  for(i in 1:nrow(data)){
-    for(col in cols_to_strip){
-      data[i, col] <- sanitize_links(data[i, col])
-    }
-  }
-  data
-}
-
 # Take a position dataframe and the section id desired
 # and prints the section to markdown. 
 print_section <- function(position_data, section_id){
@@ -71,35 +25,17 @@ print_section <- function(position_data, section_id){
       ),
       description_bullets = map_chr(descriptions, ~paste('-', ., collapse = '\n')),
     ) %>% 
-    strip_links_from_cols(c('title', 'description_bullets')) %>% 
     mutate_all(~ifelse(is.na(.), 'N/A', .)) %>% 
-    glue_data(
-      "### {title}",
-      "\n\n",
-      "{loc}",
-      "\n\n",
-      "{institution}",
-      "\n\n",
-      "{timeline}", 
-      "\n\n",
-      "{description_bullets}",
-      "\n\n\n",
-    )
-}
-
-# Construct a bar chart of skills
-build_skill_bars <- function(skills, out_of = 5){
-  bar_color <- "#969696"
-  bar_background <- "#d9d9d9"
-  skills %>% 
-    mutate(width_percent = round(100*level/out_of)) %>% 
-    glue_data(
-      "<div class = 'skill-bar'",
-      "style = \"background:linear-gradient(to right,",
-      "{bar_color} {width_percent}%,",
-      "{bar_background} {width_percent}% 100%)\" >",
-      "{skill}",
-      "</div>"
-    )
+      glue_data(
+        "### {title}",
+        "\n\n",
+        "{loc}",
+        "\n\n",
+        "{institution}",
+        "\n\n",
+        "{timeline}", 
+        "\n\n",
+        "{description_bullets}",
+        "\n\n\n")
 }
 
